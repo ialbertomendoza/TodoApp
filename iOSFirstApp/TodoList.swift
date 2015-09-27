34fdd968fb41536639c293a2acd9fcb3c79daf70
Neris <mendozaene@gmail.com>
2015-09-26 19:26:24 -0500

    Persistencia de datos

--- iOSFirstApp/TodoList.swift
+++ iOSFirstApp/TodoList.swift
@@ -10,8 +10,41 @@ import UIKit
 
 class TodoList: NSObject {
     var items: [String] = []
+    
+    // Inicializador, aquí podemos cargar recursos iniciales
+    override init() {
+        super.init()
+        loadItems()
+    }
+    
+    private let fileURL: NSURL = {
+        let fileMaganer = NSFileManager.defaultManager()
+        let documentDirectoryURLs = fileMaganer.URLsForDirectory(.DocumentDirectory,
+            inDomains: .UserDomainMask) as [NSURL] // Casting for use Swift plist struct
+        let documentDirectoryURL = documentDirectoryURLs.first!
+        print("Path de documentos"+("\(documentDirectoryURL)"))
+        return documentDirectoryURL.URLByAppendingPathComponent("todolist.items")
+    }()
+    
+    
     func addItem(item: String) {
         items.append(item)
+        saveItems()
+    }
+    
+    func saveItems() {
+        let itemsArray = items as NSArray
+        if itemsArray.writeToURL(self.fileURL, atomically: true) {
+            print("Guardado correctamente")
+        } else {
+            print("No se pudo guardar")
+        }
+    }
+    
+    func loadItems() {
+        if let itemsArray = NSArray(contentsOfURL: self.fileURL) as? [String] { // Casting
+            self.items = itemsArray
+        }
     }
 }
 
